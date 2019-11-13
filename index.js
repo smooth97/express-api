d00be5d26c5d275f8d9d3478747e3b0818db1446
smooth97 <dbtmdwls0506@naver.com>
2019-11-13 14:45:36 +0900

    get / post

---
+++ index.js
@@ -0,0 +1,53 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "courses1" },
+  { id: 2, name: "courses2" },
+  { id: 3, name: "courses3" }
+];
+app.get("/", (req, res) => {
+  res.send("Hello World~~");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+  console.log(result);
+
+  if (result.error) {
+    // 400 Bad Request
+    res.status(400).send(result.error);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+// /api/coureses/1
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send(`ID was not found`);
+  res.send(course);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
